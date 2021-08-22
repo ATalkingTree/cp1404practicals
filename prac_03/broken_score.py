c08c2144c7bc8bbe8d104cf96a87ff967d09a753
Louis-Bui <buikhanhlinhvu@gmail.com>
2021-08-22 21:06:42 +1000

    Refactor score status program to use functions

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -4,13 +4,22 @@ Broken program to determine score status
 """
 
 
-score = float(input("Enter score: "))
-if 0 > score or score > 100:
-    print("Invalid score")
-else:
-    if score >= 90:
-        print("Excellent")
-    elif score >= 50:
-        print("Passable")
+def main():
+    score = float(input("Enter score: "))
+    result = determine_score_status(score)
+    print(result)
+
+
+def determine_score_status(score):
+    if 0 > score or score > 100:
+        return "Invalid score"
     else:
-        print("Bad")
+        if score >= 90:
+            return "Excellent"
+        elif score >= 50:
+            return "Passable"
+        else:
+            return "Bad"
+
+
+main()
