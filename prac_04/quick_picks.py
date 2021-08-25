48d194b684d5048bd2b29e0e4a32f24ccaceee4c
Louis-Bui <buikhanhlinhvu@gmail.com>
2021-08-25 15:13:48 +1000

    Program now generate many quick picks based on user input

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -8,11 +8,14 @@ AMOUNT_OF_RANDOM_NUMBERS = 6
 LOWER_RANDOM_VALUE = 1
 UPPER_RANDOM_VALUE = 45
 
-quick_picks = []
-for i in range(AMOUNT_OF_RANDOM_NUMBERS):
-    random_number = random.randint(LOWER_RANDOM_VALUE, UPPER_RANDOM_VALUE)
-    while random_number in quick_picks:
+amount_of_quick_picks = int(input("How many quick picks? "))
+
+for i in range(amount_of_quick_picks):
+    quick_picks = []
+    for j in range(AMOUNT_OF_RANDOM_NUMBERS):
         random_number = random.randint(LOWER_RANDOM_VALUE, UPPER_RANDOM_VALUE)
-    quick_picks.append(random_number)
-quick_picks.sort()
-print(quick_picks)
+        while random_number in quick_picks:
+            random_number = random.randint(LOWER_RANDOM_VALUE, UPPER_RANDOM_VALUE)
+        quick_picks.append(random_number)
+    quick_picks.sort()
+    print(*quick_picks)
