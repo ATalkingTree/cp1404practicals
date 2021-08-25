08535153bbad457447a840c375cbc24316b976f8
Louis-Bui <buikhanhlinhvu@gmail.com>
2021-08-25 13:50:35 +1000

    Added inadequate security checker program

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -5,7 +5,8 @@ Intermediate exercises
 
 
 def main():
-    basic_list_operations()
+    # basic_list_operations()
+    inadequate_security_checker()
 
 
 def basic_list_operations():
@@ -19,6 +20,17 @@ def basic_list_operations():
     print(f"The smallest number is {min(number_lists)}")
     print(f"The largest number is {max(number_lists)}")
     print(f"The average of the numbers is {sum(number_lists) / len(number_lists)}")
+
+
+def inadequate_security_checker():
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
+                 'bob']
+    username = input("Enter username: ")
+    if username in usernames:
+        print("Access granted")
+    else:
+        print("Access denied")
 
 
 main()
