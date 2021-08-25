97d3ca2b94b0bb4448e659e76443de2adbcbf190
Louis-Bui <buikhanhlinhvu@gmail.com>
2021-08-25 13:31:48 +1000

    Store and unpack data in lists

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,23 +7,27 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = get_data()
-    print(data)
+    lists = get_data()
+    for subject_element in lists:
+        print("{:6} is taught by {:12} and has {:3} students".format(*subject_element))
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    lists = []
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
+        # print(line)  # See what a line looks like
+        # print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
+        # print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        # print(parts)  # See if that worked
+        lists.append(parts)
+        # print("----------")
     input_file.close()
+    return lists
 
 
 main()
