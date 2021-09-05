ee91fa69a1714b28aa6e669491caeb042b2f8073
Louis-Bui <buikhanhlinhvu@gmail.com>
2021-09-05 20:20:14 +1000

    Edited the printing format

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -12,6 +12,11 @@ for word in words:
         collection_of_words[word] += 1
     else:
         collection_of_words[word] = 1
-sorted(collection_of_words)
-for key, value in collection_of_words.items():
-    print(f"{key} : {value}")
+words = list(collection_of_words.keys())
+words.sort()
+max_length = 0
+for word in words:
+    if len(word) > max_length:
+        max_length = len(word)
+for word in words:
+    print(f"{word:{max_length}} : {collection_of_words[word]}")
