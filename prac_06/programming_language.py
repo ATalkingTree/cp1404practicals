698e7d9e8cba0ba490eae17efcf44f56ef9fb3e4
Louis-Bui <buikhanhlinhvu@gmail.com>
2021-10-04 21:36:16 +1000

    Added and initialise ProgrammingLanguage class

---
+++ prac_06/programming_language.py
@@ -0,0 +1,20 @@
+"""CP1404/CP5632 Practical - Programming Language class."""
+
+
+class ProgrammingLanguage:
+    def __init__(self, name, typing, reflection, year):
+        """Initialise a Car instance.
+
+        name: string, name of the programming language
+        typing: string, either "Dynamic" or "Static"
+        reflection: boolean, to determine if a language supports reflection or not
+        year: integer, released year of the language
+        """
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        return self.typing == "Dynamic"
+
